0ba93cdca90a20320920e700654837b1510cfedc
Steven Zhang <YZhang@lcs.on.ca>
2021-01-29 12:06:14 -0500

    Classwork half

--- perimeterAreaValueHelper/main.swift
+++ perimeterAreaValueHelper/main.swift
@@ -21,6 +21,9 @@ func areaOfRectangle(length: Double, width: Double) -> Double {
     
 }
 
+/// finds the area of a circle
+/// - Parameter radius: the distance of the center of the circle to its outer edge
+/// - Returns: area of the circle
 func areaOfCircle(radius: Double) -> Double {
     return Double.pi * pow(radius, 2.0)
 }
@@ -35,3 +38,25 @@ let area = areaOfRectangle(length: givenLength, width: givenWidth)
 
 //MARK: Output
 print("The area is \(area) square units")
+
+
+/// Find the volume of the RectangularPrism
+/// - Parameters:
+///   - length: The length of the base
+///   - width: The width of the base
+///   - height: The height of the prism
+/// - Returns: volume of teh prsim
+let length1 = 2.0
+let width1 = 4.0
+let height1 = 5.0
+
+
+func volumeOfRectangularPrism (length: Double, width: Double, height: Double) -> Double {
+    
+    return length * width * height
+
+}
+let volume = volumeOfRectangularPrism(length: length1, width: width1, height: height1)
+
+
+print("The volume of the rectangular prism is \(volume) cube units")
