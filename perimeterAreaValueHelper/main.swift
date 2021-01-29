abc6fe354bc00521a6dc03a14f75e2d08a8e1eb3
Steven Zhang <YZhang@lcs.on.ca>
2021-01-29 11:34:00 -0500

    Classwork

--- perimeterAreaValueHelper/main.swift
+++ perimeterAreaValueHelper/main.swift
@@ -9,3 +9,29 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Functions
+
+/// Finds the area of a rectangle
+/// - Parameter length: length of rectangle
+/// - Parameter width: width of rectangle
+/// - Returns: area of the rectangle
+func areaOfRectangle(length: Double, width: Double) -> Double {
+    
+    return length * width
+    
+}
+
+func areaOfCircle(radius: Double) -> Double {
+    return Double.pi * pow(radius, 2.0)
+}
+
+//MARK: Input
+let givenLength = 10.0         //Set to abouble using "typing inference"
+let givenWidth: Double = 2     //Set to Double using "typing annotation"
+
+//MARK: Process
+let area = areaOfRectangle(length: givenLength, width: givenWidth)
+
+
+//MARK: Output
+print("The area is \(area) square units")
